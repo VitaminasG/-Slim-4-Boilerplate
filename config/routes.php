c2efe5fd61789eefd65197eb357d4bf993fbe97c
Gediminas Palšys <vitaminas.g@gmail.com>
2021-09-19 02:58:34 +0300

    initial commit

---
+++ config/routes.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+return static function (App $app, $debugMode) {
+
+    // Create Twig
+    $twig = Twig::create(__DIR__ . $_ENV['TEMPLATE_PATH'], ['cache' => !$debugMode]);
+
+    // Add Twig-View Middleware
+    $app->add(TwigMiddleware::create($app, $twig));
+
+    $app->get('/', function (Request $request, Response $response, $args) {
+        $view = Twig::fromRequest($request);
+        return $view->render($response, 'home.twig');
+    })->setName('home');
+};
